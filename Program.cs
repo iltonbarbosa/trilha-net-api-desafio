4b798e246b925d325dedb31d8aaafe85d8ccecbc
Ilton Barbosa <jiltonbarbosa@gmail.com>
2023-08-04 17:05:49 -0300

    desafio concluído

--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
+var serverVersion = new MySqlServerVersion(new Version(10, 4, 24));
 
 // Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+	options.UseMySql(builder.Configuration.GetConnectionString("ConexaoPadrao"),serverVersion));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
